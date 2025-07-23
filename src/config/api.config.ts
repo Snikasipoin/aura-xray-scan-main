c60d5dfe78eda5d8aaded77b8e44a890ad8b517b
Roman Kalmykow <snikasipoin@gmail.com>
2025-07-23 16:43:22 +0600

    edit url api

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -2,7 +2,7 @@
 import { ApiConfig } from '../types/api';
 
 // Конфигурация по умолчанию для API
-const DEFAULT_API_URL = 'https://snikasipoin-api-xray-scan-ed4e.twc1.net'; // URL API бэкенда
+const DEFAULT_API_URL = 'https://snikasipoin-api-xray-scan-f4ce.twc1.net/'; // URL API бэкенда
 
 // Получаем URL API из локального хранилища или используем значение по умолчанию
 export const getApiConfig = (): ApiConfig => {
